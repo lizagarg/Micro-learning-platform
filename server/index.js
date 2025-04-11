5498d50da4523de3537fe3d505eac8734ad51062
Liza Garg <115346164+lizagarg@users.noreply.github.com>
2025-04-12 01:24:29 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,10 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ['https://micro-learning-platform-frontend.onrender.com/', 'http://localhost:5173'], // update with your actual frontend domain
+  credentials: true
+}));
 app.use(morgan('dev'));
 
 // Database connection
